8eed4ee91077c791a8be43d94fa3997ec1bbac39
Iulia-Georgiana Talpalariu <iulia-georgiana.talpalariu@s.unibuc.ro>
2023-05-22 13:07:09 +0300

    m

--- e-ticketing-platform/src/Main.java
+++ e-ticketing-platform/src/Main.java
@@ -1,12 +1,4 @@
 import dbconfig.DatabaseConfiguration;
-import dtos.EventDto;
-import dtos.LocationDto;
-import dtos.UserDto;
-import enums.EventType;
-import enums.LocationType;
-import enums.TicketCategory;
-import exceptions.UserNameNotFoundException;
-import models.*;
 import services.EventService;
 import services.LocationService;
 import services.TicketService;
@@ -16,22 +8,18 @@ import services.impl.LocationServiceImpl;
 import services.impl.TicketServiceImpl;
 import services.impl.UserServiceImpl;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
-import static constants.Constants.USERNAME_NOT_FOUND;
-
 public class Main {
     public static void main(String[] args)
     {
+        System.out.println("Initializare DB si servicii:");
+
         DatabaseConfiguration databaseConfiguration = new DatabaseConfiguration();
         LocationService locationService = new LocationServiceImpl(databaseConfiguration);
         EventService eventService = new EventServiceImpl(locationService, databaseConfiguration);
         TicketService ticketService = new TicketServiceImpl(eventService, databaseConfiguration);
         UserService userService = new UserServiceImpl(ticketService, databaseConfiguration);
+
+        System.out.println("get in toate serviciile:");
 
         System.out.println(userService.getUsers());
         System.out.println(ticketService.getTickets());
